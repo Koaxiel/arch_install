0dafedf90d55de24d6bc27da45d28d3f6214c2b0
Robin Douine <r.douine@criteo.com>
2021-06-12 14:42:30 +0200

    Allow the choice of the install disk

--- arch_install.sh
+++ arch_install.sh
@@ -4,5 +4,7 @@ read -s -p "Root password: " password
 printf "\n"
 read -p "Hostname: " hostname
 printf "\n"
+read -p "Disk to use (sdx): " disk
+printf "\n"
 # Run Command
-ansible-playbook -i arch_install/inventory -vvv -e "root_password=$password hostname=$hostname" arch_install/main.yml
+ansible-playbook -i arch_install/inventory -vvv -e "root_password=$password hostname=$hostname disk=$disk" arch_install/main.yml
